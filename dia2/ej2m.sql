0cb73647f7ca1d9bb0c5b2cd4aa9a3dd4774b14e
Matias Dvorsky <matias.dvorsky@mercadolibre.com>
2023-04-24 12:35:59 -0300

    Update ej2m.sql

--- dia2/ej2m.sql
+++ dia2/ej2m.sql
@@ -29,3 +29,49 @@ SELECT Nombre, ApellidoFROM estudiante WHERE Carrera = 'Informática';
 
 /* 4 */
 SELECT Nombre FROM autor WHERE Nacionalidad = 'Francesa' OR Nacionalidad = 'Italiana';
+
+/* 5 */
+SELECT * FROM libro WHERE Area != 'Internet';
+
+/* 6 */
+SELECT * FROM libro WHERE Editorial = 'Salamandra';
+
+/* 7 */
+SELECT * FROM estudiante WHERE Edad > (SELECT AVG(Edad) FROM estudiante);
+
+/* 8 */
+SELECT Nombre FROM estudiante WHERE apellido LIKE 'G%';
+
+/* 9 */
+SELECT autor.Nombre FROM autor
+JOIN libroautor ON autor.idAutor = libroautor.idAutor
+JOIN libro ON libro.idLibro = libroautor.idLibro
+WHERE libro.Titulo = 'El Universo: Guía de viaje';
+
+/* 10 */
+SELECT libro.Titulo FROM libro
+JOIN libroautor ON libro.idLibro = libroautor.idLibro
+JOIN prestamo ON libroautor.idLibro = prestamo.idLibro
+JOIN estudiante ON prestamo.idLector = estudiante.idLector
+WHERE estudiante.Nombre = 'Filippo' AND estudiante.Apellido = 'Galli';
+
+/* 11 */
+SELECT Nombre FROM estudiante ORDER BY Edad LIMIT 1;
+
+/* 12 */
+SELECT estudiante.Nombre FROM estudiante
+JOIN prestamo ON estudiante.idLector = prestamo.idLector
+JOIN libro on prestamo.idLibro = libro.idLibro
+WHERE libro.Area = 'Base de Datos';
+
+/* 13 */
+SELECT libro.Titulo FROM libro
+JOIN libroautor ON libro.idLibro = libroautor.idLibro
+JOIN autor ON libroautor.idAutor = autor.idAutor
+WHERE autor.Nombre = 'J.K Rowling';
+
+/* 14 */
+SELECT libro.Titulo FROM libro
+JOIN prestamo ON libro.idLibro = prestamo.idLibro
+WHERE prestamo.FechaDevolucion = '2021-07-16';
+
