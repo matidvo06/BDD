93253aaad8a1debc54c4b9ede4fa357ad685a555
Matias Dvorsky <matias.dvorsky@mercadolibre.com>
2023-04-26 13:09:50 -0300

    Update main.go

--- implementacion/ejemplo_completo1/cmd/server/main.go
+++ implementacion/ejemplo_completo1/cmd/server/main.go
@@ -8,7 +8,39 @@ import (
 )
 
 func main() {
+	// Open database connection.
+	databaseConfig := &mysql.Config{
+		User:      "root",
+		Passwd:    "gormit897",
+		Addr:      "localhost:3306",
+		DBName:    "storage",
+		ParseTime: true,
+	}
 
+	database, err := sql.Open("mysql", databaseConfig.FormatDSN())
+	if err != nil {
+		panic(err)
+	}
+
+	// Ping database connection.
+	if err = database.Ping(); err != nil {
+		panic(err)
+	}
+
+	println("Database connection established")
+
+	// Create products repository.
+	var repository products.Repository = &products.MySQLRepository{
+		Database: database,
+	}
+
+	// Get product.
+	product, err := repository.Get(1)
+	if err != nil {
+		panic(err)
+	}
+	
+	
 	storage := store.NewJsonStore("./products.json")
 
 	repo := product.NewRepository(storage)
